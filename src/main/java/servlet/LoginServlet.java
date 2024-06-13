2b283ae76962328afb82750f774303285235e67e
cebuka1 <ebusco29@gmail.com>
2024-06-13 13:43:39 +0100

    E-commerce simplified website

---
+++ src/main/java/servlet/LoginServlet.java
@@ -0,0 +1,43 @@
+package servlet;
+
+import connection.Dao.UserDao;
+import connection.DbCon;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.User;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+
+@WebServlet("/user-login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		try (PrintWriter out = response.getWriter()) {
+			String email = request.getParameter("login-email");
+			String password = request.getParameter("login-password");
+
+			UserDao udao = new UserDao(DbCon.getConnection());
+			User user = udao.userLogin(email, password);
+			if (user != null) {
+				request.getSession().setAttribute("auth", user);
+//				System.out.print("user logged in");
+				response.sendRedirect("index.jsp");
+			} else {
+				out.println("there is no user");
+			}
+
+		} catch (ClassNotFoundException|SQLException e) {
+			e.printStackTrace();
+		} 
+
+	}
+}
